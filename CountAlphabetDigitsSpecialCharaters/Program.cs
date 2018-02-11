6b855adf8b54eeafd2680b33246ce5ea32820349
Bishnu Gopali <bishnugopali@hotmail.com>
2018-02-11 16:33:23 +0000

    Count alphabet digits and special characters

--- CountAlphabetDigitsSpecialCharaters/Program.cs
+++ CountAlphabetDigitsSpecialCharaters/Program.cs
@@ -10,6 +10,43 @@ namespace CountAlphabetDigitsSpecialCharaters
     {
         static void Main(string[] args)
         {
+            string str;
+            int alp, digit, splch, i, l;
+            alp = digit = splch = i = 0;
+
+
+            Console.Write("\n\nCount total number of alphabets, digits and special characters :\n");
+            Console.Write("--------------------------------------------------------------------\n");
+            Console.Write("Input the string : ");
+            str = Console.ReadLine();
+            l = str.Length;
+
+            /* Checks each character of string*/
+
+            for(i=0;i<l;i++)
+            {
+                if((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+                {
+                    alp++;
+                }
+                else if(str[i] >= '0' && str[i] <= '9')
+                {
+                    digit++;
+                }
+                else
+                {
+                    splch++;
+                }
+
+            }
+
+            Console.Write("Number of Alphabets in the string is : {0}\n", alp);
+            Console.Write("Number of Digits in the string is : {0}\n", digit);
+            Console.Write("Number of Special characters in the string is : {0}\n\n", splch);
+
+            Console.ReadLine();
+
+
         }
     }
 }
